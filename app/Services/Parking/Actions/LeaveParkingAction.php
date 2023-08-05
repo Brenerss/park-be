9874d739bbcd70adebda4fe3049c47fc68fc157d
Brener Sousa <brener.dev@gmail.com>
2023-08-05 13:06:01 -0300

    feat: leave parking actions throws exception if already leaved and bind vehicle class by type

--- app/Services/Parking/Actions/LeaveParkingAction.php
+++ app/Services/Parking/Actions/LeaveParkingAction.php
@@ -2,15 +2,33 @@
 
 namespace App\Services\Parking\Actions;
 
+use App\Enums\VehicleType;
 use App\Models\Parking;
 use App\Services\Parking\Price\CarPrice;
+use App\Services\Parking\Price\MotocyclePrice;
+use App\Services\Parking\Price\TruckPrice;
 use Carbon\Carbon;
+use Exception;
 
 class LeaveParkingAction
 {
+    /**
+     * @throws Exception
+     */
     public function execute(Parking $parking): Parking
     {
-        $price = (new CarPrice($parking))->calculate();
+        if ($parking->exit_time) {
+            Throw new Exception('Car already leaved');
+        }
+
+        $class = match ($parking->vehicle->type) {
+            VehicleType::CAR => CarPrice::class,
+            VehicleType::TRUCK => TruckPrice::class,
+            VehicleType::MOTORCYCLE => MotocyclePrice::class,
+            default => null,
+        };
+
+        $price = $class ? (new $class($parking))->calculate() : 0;
 
         $parking->update([
             'price' => $price,
